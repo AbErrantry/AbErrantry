b5a522a30ec32811639998b6b5ec5c38a1f3e608
smcroskey <smcroskey@gmail.com>
2017-11-24 22:46:07 -0500

    Removed Anima2D and added level streaming
    Since we are now going with spritesheets for animation, Anima2D is no longer needed in the project.
    
    I've added a level streaming system which will unload/load two adjacent levels seamlessly. All of game objects not related to an individual level (i.e. the player, canvas, and event system) have been moved to a new, persistent level. This level contains a bunch of hallways which will connect each of the individual levels in the game.
    
    Since these hallways are pretty small, the performance impact should be minimal. These hallways also contain a loading/unloading mechanism consisting of 6 trigger colliders. The outer two trigger colliders unload the level on the opposite side if it is not already unloaded. The next two interior colliders load the level on the opposite side if it is not already loaded. The middle two colliders ensure that both levels are loaded before the player continues. If not, then the player will have to wait until they are loaded. This part has not been implemented yet.
    
    The next update from me will move all of the input from the player to one function in order to generalize the movement/attack scripts so that they can be used for AI as well.

---
+++ sandbox/Assets/Scripts/LevelStreaming/LevelStreamManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelStreamManager : MonoBehaviour
+{
+    public string sceneName;
+
+    private AsyncOperation asyncLoad;
+    private AsyncOperation asyncUnload;
+
+    public bool isLoaded;
+    public Scene scene;
+
+    //used for initialization
+    void Start()
+    {
+        scene = SceneManager.GetSceneByName(sceneName);
+        isLoaded = scene.isLoaded;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Knight")
+        {
+            if(!isLoaded)
+            {
+                //TODO: add input reference here to disable input until level is loaded
+                //maybe a delegate to the AsyncOperation.complete
+                    //when it fires, reenable input. 
+                //edge case for low-end systems
+                Debug.Log("Error. " + sceneName + " is not loaded yet. Disable input and wait until loaded." + isLoaded);
+            }
+        }
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        asyncLoad.allowSceneActivation = true;
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        isLoaded = true;
+    }
+
+    IEnumerator UnloadSceneAsync()
+    {
+        asyncUnload = SceneManager.UnloadSceneAsync(sceneName);
+        while (!asyncUnload.isDone)
+        {
+            yield return null;
+        }
+        isLoaded = false;
+    }
+
+    public void LoadScene()
+    {
+        if (!isLoaded)
+        {
+            StopAllCoroutines();
+            StartCoroutine(LoadSceneAsync());
+        }
+    }
+
+    public void UnloadScene()
+    {
+        if (isLoaded)
+        {
+            StopAllCoroutines();
+            StartCoroutine(UnloadSceneAsync());
+        }
+    }
+}
---
+++ sandbox/Assets/Scripts/LevelStreaming/LevelUnloadTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUnloadTrigger : MonoBehaviour
+{
+    public LevelStreamManager manager;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Knight")
+        {
+            manager.UnloadScene();
+        }
+    }
+}
