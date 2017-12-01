e2f1220ebb58ed3e690da6723ddd6f976878574e
Aristotelis Papaioannou <adpapaio@gmail.com>
2017-12-01 13:41:54 -0500

    Started the AI functionality, Switched ground trigger.

--- sandbox/Assets/Scripts/Character/Input/BehaviorAI.cs
+++ sandbox/Assets/Scripts/Character/Input/BehaviorAI.cs
@@ -6,7 +6,10 @@ namespace Character2D
 {
     public class BehaviorAI : MonoBehaviour
     {
-        CharacterMovement characterMovement;
+        public CharacterMovement aiMovement;
+        public CharacterInteraction aiInteraction;
+
+
         // Use this for initialization
         void Start()
         {
@@ -16,7 +19,16 @@ namespace Character2D
         // Update is called once per frame
         void Update()
         {
-
+            if (Time.timeScale == 1)
+            {
+                aiMovement.jumpInput = true; //send jump input
+                /*
+                cm.crouchInput = CrossPlatformInputManager.GetButton("Fire1"); //send crouch input
+                cm.runInput = CrossPlatformInputManager.GetButton("Fire3"); //send run input
+                cm.mvmtSpeed = CrossPlatformInputManager.GetAxis("Horizontal"); //send movement speed
+                ci.interactionInput = CrossPlatformInputManager.GetButtonDown("Fire2"); //send interaction input
+                */
+            }
         }
     }
 }
--- sandbox/Assets/Scripts/Character/Managers/GroundedManager.cs
+++ sandbox/Assets/Scripts/Character/Managers/GroundedManager.cs
@@ -7,19 +7,19 @@ namespace Character2D
     public class GroundedManager : MonoBehaviour
     {
         [SerializeField] public List<GameObject> currentGround; //list of objects the character is standing on
-        private CharacterMovement characterMovement; //reference to the character movement script
-
+        public CharacterMovement characterMovement; //reference to the character movement script
+        public string tag;
         //used for initialization
         void Start()
         {
-            characterMovement = GameObject.Find("Knight").GetComponent<CharacterMovement>();
+
         }
 
         //detects when the player is standing on a new object
         private void OnTriggerEnter2D(Collider2D other)
         {
             //if the object is not part of the player,
-            if (other.tag != "Player")
+            if (other.tag != tag)
             {
                 //set the player to be grounded and add the object to the list
                 characterMovement.isGrounded = true;
@@ -31,7 +31,7 @@ namespace Character2D
         private void OnTriggerExit2D(Collider2D other)
         {
             //if the object is not part of the player,
-            if (other.tag != "Player")
+            if (other.tag != tag)
             {
                 //remove the object from the list
                 currentGround.Remove(other.gameObject);
