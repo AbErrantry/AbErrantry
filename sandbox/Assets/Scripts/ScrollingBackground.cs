f1983f059c3e578d1716ff6328ebdc7ab02c4a01
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-01-10 00:21:49 -0500

    Added The main Background for the First Level
    Also started to create the Parallax Scrolling. Will Finish tomorrow.

---
+++ sandbox/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,27 @@
+﻿using Character2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float speed = 0;
+    public CharacterMovement player;
+    private Vector2 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        speed = player.mvmtSpeed / 4;
+        if (speed == 0)
+            speed = 1;
+        offset = new Vector2(Time.deltaTime * speed, 0);
+
+        this.GetComponent<MeshRenderer>().material.mainTextureOffset = offset;
+	}
+}
