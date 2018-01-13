5bc51d524b2ede2c72734ff06f1ecc8972dd5b37
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-01-13 18:31:50 -0500

    Camera shift
    Camera shifts when player opens back pack,
    
    currently it always checks for the player to have the menu open but i
    wanna change this so that when the player presses to open the backpack
    it shifts. (saves processing power)

---
+++ sandbox/Assets/CameraShift.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Cinemachine;
+using Character2D;
+
+public class CameraShift : MonoBehaviour {
+
+    public CinemachineVirtualCamera cineCamera;
+    public PlayerInput player;
+    public float shiftedX;
+    public float unshiftedX;
+
+    private void LateUpdate()
+    {
+        if (!player.acceptInput)
+        {
+            cineCamera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = shiftedX;
+        }
+        else
+        {
+            cineCamera.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = unshiftedX;
+        }
+    }
+}
