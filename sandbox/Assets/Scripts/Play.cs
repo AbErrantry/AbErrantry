ee63edef8152f990429cb54315cfb31cd1fd7957
PachecoLuke <ltpachec@umich.edu>
2018-01-15 21:25:00 -0500

    Code for Play game (argumentException preventing)

---
+++ sandbox/Assets/Scripts/Play.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Play : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Button>().onClick.AddListener(() => LoadGame());
+	}
+	
+	// Update is called once per frame
+	void LoadGame() {
+        SceneManager.LoadScene("Level1-SC");
+	}
+}
