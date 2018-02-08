1ecad9910d30b0705eda70d2daab494d33981f07
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-02-08 17:33:17 -0500

    Made a Background switcher[needs update]
    So the script to change the background works, but the issue is now the
    background wont move with the camera. Im trying to stay away for
    activating/deactivating meshes but I might end up doing that.

---
+++ sandbox/Assets/Scripts/Camera/BackgroundSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSwitch : MonoBehaviour {
+
+	
+
+	[System.Serializable]
+	public class Backgrounds
+	{
+		public MeshRenderer[] backImages;
+		public float[] speed;
+	}
+
+	public ScrollingBackground scroll;
+	public Backgrounds[] background;
+	public int newSize;
+
+
+	public void UpdateScrolling(int element) //element aka level
+	{
+		element--;//make it work for the array, going to change this
+
+		newSize = background[element].backImages.Length;
+		scroll.ScrollChange(newSize, background[element].backImages, background[element].speed);
+	}
+}
--- sandbox/Assets/Scripts/Camera/ScrollingBackground.cs
+++ sandbox/Assets/Scripts/Camera/ScrollingBackground.cs
@@ -15,11 +15,13 @@ public class ScrollingBackground : MonoBehaviour
 
     private Vector3 currPos;
     private Vector3 prevPos;
+
     // Use this for initialization
     void Start()
     {
         currPos = cam.transform.position;
         offsets = new Vector2[Backgrounds.Length];
+        UpdateBackground();
     }
 
     // Update is called once per frame
@@ -27,16 +29,34 @@ public class ScrollingBackground : MonoBehaviour
     {
         prevPos = currPos;
         currPos = cam.transform.position;
-
-
+    
         if (cam.velocity.x != 0 && (currPos != prevPos))
         {
-            for(int i = 0; i < Backgrounds.Length; i++)
-            {
-                speed = cam.velocity.x / BackgroundSpeed[i];
-                offsets[i] += new Vector2(Time.deltaTime * speed, 0);
-                Backgrounds[i].material.mainTextureOffset = offsets[i];
-            }
+             UpdateBackground();
+        }
+    }
+
+    public void UpdateBackground()
+    {
+        for(int i = 0; i < Backgrounds.Length; i++)
+                {
+                    speed = cam.velocity.x / BackgroundSpeed[i];
+                    offsets[i] += new Vector2(Time.deltaTime * speed, 0);
+                    Backgrounds[i].material.mainTextureOffset = offsets[i];
+                    
+                }
+    }
+
+    public void ScrollChange(int newSize, MeshRenderer[] newImages, float[] newSpeeds)
+    {
+        Backgrounds = new MeshRenderer[newSize];
+        BackgroundSpeed = new float[newSize];
+        offsets = new Vector2[newSize];
+
+        for(int i=0; i <newSize; i++)
+        {
+            Backgrounds[i] = newImages[i];
+            BackgroundSpeed[i] = newSpeeds[i];
         }
     }
 }
