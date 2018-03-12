a786c88be77eb10b600b1cf05f28cb08b3f1ecf3
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-03-11 22:00:38 -0400

    Water kills now
    Fixed a tiling bug on the material and made it so Water kills knighty,
    will make it kill all attackables later (wanna make it so crates sink
    but enemies die). Also Crates now float for some odd reason

---
+++ sandbox/Assets/Scripts/Interaction/Drowning.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character2D
+{
+public class Drowning : MonoBehaviour {
+
+
+	private void OnTriggerEnter2D(Collider2D coll)
+    {
+		if(coll.tag == "Player")
+		{
+			Attackable player = coll.gameObject.GetComponent<Attackable>();
+
+			if(!player.isDying)
+			{
+        		player.Kill();
+			}
+		}
+    }
+
+ }
+}
