9608507f1acef4bcfe62e6ac94e8719c84d6eaf1
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-03-11 21:32:28 -0400

    Poseidon has Joined
    Added the Water and the Boats
    
    Water:
    It is a prefab with a default background, we will have to scale
    accordingly for the areas(shouldnt be an issue). The Desnity of the
    water is 25 so when the player jumps on it it is still dense enough to
    support that. Crates are way to heavy to be able to float (can probably
    use that for a puzzle). The background on the water changes based on the
    waters magnitude, it gives the illusion of movement.
    WATER DOES NOT KILL AS OF NOW, I wanted to push this before I broke
    anything.
    Boat:
    The boat is a basic collider with the boat image, Its mass is 9 so when
    the player goes on it, it will become 10. This helps the boat stay above
    water.  One issue is that the player slides off.
    
    Also added a single spike so we can attach them to the metal platforms.

---
+++ sandbox/Assets/Scripts/Platforms/WaterParallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterParallax : MonoBehaviour {
+
+[Tooltip("No need to set this, it is done inside the script. Viewing is for debugging purposes.")]
+	public float speed;
+	private Vector2 offset;
+
+	private MeshRenderer water;
+	private BuoyancyEffector2D buoy;
+
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 0;
+		water = this.gameObject.GetComponent<MeshRenderer>();
+		buoy = this.gameObject.GetComponent<BuoyancyEffector2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		speed = (buoy.flowMagnitude/500) * -1; //Multiplied by -1 so it goes the opposite way of the magnitude, giving the illusion of it moving together.
+        offset += new Vector2(Time.deltaTime * speed, 0);
+        water.material.mainTextureOffset = offset;
+	}
+}
