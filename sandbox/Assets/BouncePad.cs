b58c71649657b5223796224a2f8dc5d35caf5900
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-03-14 00:28:07 -0400

    Started Implementing Bounce Pad
    Missing:
    Animation

---
+++ sandbox/Assets/BouncePad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour {
+
+	private Rigidbody2D playerRBody;
+
+	[Tooltip("Set the Force of the Bounce pad. Is multiplied by 100 in script.")]
+	[Range(0,10)]
+	public float bounceForce;
+
+	void Start()
+	{
+		bounceForce *= 100;
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			playerRBody = col.gameObject.GetComponent<Rigidbody2D>();
+			Vector2 playerPos = col.transform.position;
+			Vector2 dir = col.contacts[0].point - playerPos;
+         	dir = -dir.normalized;
+        	playerRBody.AddForce(dir*bounceForce);
+		}
+	}
+}
