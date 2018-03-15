b3f4afc3a40a2fc026c67bd5c9779bf899180972
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-03-15 03:36:13 -0400

    Almost done with the Main Forest Level
    Had to stop because my mouse broke.
    Also created an object looping script for the items in the water (boats,
    crates, etc.) it seems to be working fine if the boats are moving right
    to left but not the other way around, I will look into this.
    
    Issues with the level currently:
    -First Vertical platform doesnt move when the button is pressed.
    - hatch in first area is not locked on start
    - Water looks weird
    - Dont really know how to make the platform puzzle in the third part.

---
+++ sandbox/Assets/WaterObjRepeater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterObjRepeater : MonoBehaviour {
+
+	public bool isLeft;
+	public GameObject OtherSide;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(isLeft)
+		{
+			col.transform.position = new Vector2(OtherSide.transform.position.x + col.bounds.size.x, OtherSide.transform.position.y);
+		}
+		else
+		{
+			col.transform.position = new Vector2(OtherSide.transform.position.x - col.bounds.size.x, OtherSide.transform.position.y);
+		}
+	
+	}
+}
