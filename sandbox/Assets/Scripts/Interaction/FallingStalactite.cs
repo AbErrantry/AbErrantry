8bbb7a0ff0cdc24d997fed1ad534cb24d9bebf17
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-03-21 15:03:51 -0400

    Renamed the script and edited it a little

---
+++ sandbox/Assets/Scripts/Interaction/FallingStalactite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingStalactite : MonoBehaviour {
+
+	public float dropTime;
+	public bool isDropping;
+	public GameObject itemToDrop;
+	private float time;
+	// Use this for initialization
+	void Start ()
+	{
+		time = dropTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(time<= 0)
+		{
+			time = dropTime;
+			DropATite();
+		}
+		else
+		{
+			time -= Time.deltaTime;
+		}
+	}
+
+	void DropATite()
+	{
+		
+	}
+}
