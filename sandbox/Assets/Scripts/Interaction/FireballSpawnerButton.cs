93a3e65c6f96cc9a2b78d27d18e3e86dacd6df83
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-03-27 03:26:52 -0400

    Spooky Level Changes
    First Intro puzzle done
    First main puzzle almost done(need to put doors in DB).
    Third puzzle almost done
    
    Note about Fireball Spawners: I made them work with buttons.

---
+++ sandbox/Assets/Scripts/Interaction/FireballSpawnerButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character2D
+{
+public class FireballSpawnerButton : MonoBehaviour {
+
+	public GameObject[] spawners;
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.GetComponent<Player>() != null)
+		{
+			foreach(GameObject x in spawners)
+			{
+				StartCoroutine(x.GetComponent<FireballSpawner>().SpawnAnObject());
+			}
+		}
+	}
+}
+}
