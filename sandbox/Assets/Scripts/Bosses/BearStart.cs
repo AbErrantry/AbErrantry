ff491dcc55ba6d3f82b0e9cdaa0745d971eb2a6b
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-03-29 02:03:10 -0400

    BearChase is 10X more intense
    Just need to make Camera Target the Player after the chase, as well as
    fix one of the puzzle that is now impossible since crates break on your
    head.

--- sandbox/Assets/Scripts/Bosses/BearStart.cs
+++ sandbox/Assets/Scripts/Bosses/BearStart.cs
@@ -8,19 +8,23 @@ public class BearStart : MonoBehaviour {
 
     public GameObject bear;
 	public DestroyAttackablesUnlockAction destDoor;
+	public FollowTarget followTarget;
 	public Transform spawnPOS;
 	public EnemyMovement movement;
 	public GameObject endBearcon;
 	public GameObject tilemap;
 
+
     public void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(collision.gameObject.name == "Knight")
 		{
+			followTarget = GameObject.Find("CameraTarget").GetComponent<FollowTarget>();
 			GameObject clone = Instantiate(bear, spawnPOS.position, Quaternion.identity);
 			clone.GetComponentInChildren<TileDestroyer>().tilemapGameObject = tilemap;
 			clone.GetComponent<Enemy>().beacCon.beacons[0] = endBearcon;
 			destDoor.attackables[0] = clone.GetComponent<Attackable>();
+			followTarget.target = clone.transform;
 			Destroy(this.gameObject);
 		}
 	}
