2fb3b8341c7c220729d2ef4796d033a772f3cc17
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-04-01 18:10:42 -0400

    Lava Level Shell, Created Fireballs, and Lava Sprite

---
+++ sandbox/Assets/Scripts/Interaction/LavaBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character2D
+{
+public class LavaBall : MonoBehaviour {
+
+	public int damage;
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.GetComponent<Attackable>() != null)
+		{
+			col.gameObject.GetComponent<Attackable>().TakeDamage(gameObject,damage);
+		}
+	}
+
+}
+}
