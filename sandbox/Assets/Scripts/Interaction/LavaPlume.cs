e71461a86fe0eabf500541291fd6dea791d3b842
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-04-01 20:29:28 -0400

    Lava Plumes, Lava Balls, Lava Intro mechanics done
    Created Lava Plumes and a thing that can hold them if need.
    
    Made Laval Balls that will jump up and down (Just reused the Platform
    Script)
    
    Lava Intro mechanics are done.

---
+++ sandbox/Assets/Scripts/Interaction/LavaPlume.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaPlume : MonoBehaviour {
+
+	public float delay;
+	private float time;
+
+	// Use this for initialization
+	void Start ()
+	{
+		time = delay;
+		gameObject.GetComponent<Animator>().SetFloat("Delay", time);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(time<=0)
+		{
+			gameObject.GetComponent<Animator>().SetFloat("Delay", time);
+			time = delay;
+			
+		}
+		else
+		{
+			time -= Time.deltaTime;
+			gameObject.GetComponent<Animator>().SetFloat("Delay", time);
+		}
+	}
+}
