ade5c13016a35cc45f20410390a100cc61524623
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-04-04 02:46:15 -0400

    Fixed some minor animation issues with the Golem

---
+++ sandbox/Assets/Scripts/AI/GolemHandAttack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Character2D
+{
+	public class GolemHandAttack : MonoBehaviour
+	{
+
+		public GolemBoss golem;
+		public void OnTriggerEnter2D(Collider2D col)
+		{
+			if(col.gameObject.GetComponent<Attackable>() != null)
+			{
+				golem.ApplyDamage(col.gameObject);
+			}
+		}
+	}
+}
--- sandbox/Assets/Scripts/Bosses/GolemBoss.cs
+++ sandbox/Assets/Scripts/Bosses/GolemBoss.cs
@@ -2,28 +2,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace Character2D
+{
 public class GolemBoss : Boss
 {
+	public Collider2D hand1;
+	public Collider2D hand2;
+	public Collider2D eyes;
+		[Range(0,30)]
+	public float attackCooldown;
+
+	public int damage;
+	private float cooldown;
+	//public int attackPicked;
+	public bool isAttacking;
 	protected new void Start()
 	{
 		name = "Golem";
-		base.Start();
+		cooldown = attackCooldown;
 		canTakeDamage = true;
+		base.Start();
 	}
 
 	protected new void Update()
 	{
+		
+		if(cooldown<=0 && !isAttacking)
+		{
+			isAttacking=true;
+			if((currentVitality/maxVitality)*100 >= 75)
+			{
+				PickAttack(2);
+			}
+			else if((currentVitality/maxVitality)*100 >= 50)
+			{
+				PickAttack(2);
+			}
+			else
+			{
+				PickAttack(2);
+			}
+			cooldown = attackCooldown;
+			
+		}
+		else if(isAttacking)
+		{
+			cooldown = attackCooldown;
+		}
+		else
+		{
+			cooldown -= Time.deltaTime;
+		}
+	}
 
+	
+	public void PickAttack(int attackLevel)
+	{
+		
+		switch(Random.Range(0, attackLevel +1))
+		{
+			case 0:
+			Smash();
+			break;
+			case 1:
+			SwipeDown();
+			break;
+			case 2:
+			SwipeUp();
+			break;
+		}
+		
 	}
 
 	private void SwipeUp()
 	{
-		anim.Play("SwipeUp");
+		anim.Play("UpSwipe");
 	}
 
 	private void SwipeDown()
 	{
-		anim.Play("SwipeDown");
+		anim.Play("DownSwipe");
 	}
 
 	private void Smash()
@@ -40,4 +98,10 @@ public class GolemBoss : Boss
 	{
 		BossDefeated();
 	}
+
+	public void ApplyDamage(GameObject target)
+	{
+		target.GetComponent<Attackable>().TakeDamage(gameObject, damage);
+	}
+}
 }
