41f61fd014606ffb1481fe47df07e606900658b7
Aristotelis Papaioannou <adpapaio@gmail.com>
2018-04-10 15:14:07 -0400

    Wraith spawns in with Trigger

---
+++ sandbox/Assets/Scripts/Bosses/Wraith/WraithStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character2D
+{
+public class WraithStart : MonoBehaviour 
+	{
+		public Collider2D rFTrigger;
+		public GameObject wraithPrefab;
+
+		void Start () 
+		{
+			
+		}
+		public void OnTriggerEnter2D(Collider2D col)
+		{
+			if(col.gameObject.GetComponent<Player>() != null)
+			{
+				GameObject clone = Instantiate(wraithPrefab, new Vector3( GetComponent<BoxCollider2D>().bounds.max.x, GetComponent<BoxCollider2D>().bounds.min.y, wraithPrefab.transform.position.z)
+											,Quaternion.identity);
+
+				clone.GetComponent<WraithBoss>().rainFireTrigger = rFTrigger;
+			}
+		}
+	}
+}
