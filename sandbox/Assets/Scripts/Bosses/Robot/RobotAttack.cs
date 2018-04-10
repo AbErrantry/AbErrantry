4be7d43c8797cb469d25027a36d7520e55828c9c
Aristotelis Papaioannou <adpapaio@gmail.com>
2018-04-10 13:22:44 -0400

    Started Creating the Robot Boss

---
+++ sandbox/Assets/Scripts/Bosses/Robot/RobotAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character2D
+{
+	public class RobotAttack : MonoBehaviour 
+	{
+
+		public RobotBoss robot;
+
+		public void Start()
+		{
+			robot = gameObject.GetComponentInParent<RobotBoss>();
+		}
+		public void OnTriggerEnter2D(Collider2D col)
+		{
+			if(col.gameObject.GetComponent<Attackable>() != null)
+			{
+				robot.ApplyDamage(col.gameObject);
+			}
+		}
+	}
+}
---
+++ sandbox/Assets/Scripts/Bosses/Robot/RobotBoss.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character2D
+{
+public class RobotBoss : Boss {
+
+	public Collider2D attackTrigger;
+	[Range(0, 30)]
+	public float attackCooldown;
+
+	public int damage;
+	private float cooldown;
+	public bool isAttacking;
+
+	// Use this for initialization
+	protected new void Start ()
+	{
+		name = "Robot";
+		canTakeDamage = true;
+		isFacingRight = false;
+		player = GameObject.Find("Knight").GetComponent<Transform>();
+
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected new void Update () 
+	{
+		
+	}
+
+	private void Spawn()
+	{
+		anim.Play("Spawn");
+	}
+
+	protected override void InitializeDeath()
+	{
+		anim.Play("Death");
+	}
+
+	public override void FinalizeDeath()
+	{
+		BossDefeated();
+	}
+
+	public void ApplyDamage(GameObject target)
+		{
+			target.GetComponent<Attackable>().TakeDamage(gameObject, damage);
+		}
+}
+}
