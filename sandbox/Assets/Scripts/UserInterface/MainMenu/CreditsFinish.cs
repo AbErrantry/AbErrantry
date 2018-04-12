81dce862d71ac2a92acf47cef03fb905b492d116
smcroskey <smcroskey@gmail.com>
2018-04-12 13:04:42 -0400

    Credits tweaks
    The background of the credits scene now scales to any resolution.
    
    The credits now load the main menu after completion. Sped up the beginning of the sequence and moved the "Thanks for Playing" to end by itself.

---
+++ sandbox/Assets/Scripts/UserInterface/MainMenu/CreditsFinish.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsFinish : MonoBehaviour
+{
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
