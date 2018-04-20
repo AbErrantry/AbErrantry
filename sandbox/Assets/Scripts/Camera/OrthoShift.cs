5278e6ba115b1158873b2de225cdd7ddd689adff
Aristotelis Papaioannou <adpapaio@umich.edu>
2018-04-19 22:03:08 -0400

    Added OrthoZooms to all Levels
    Also fixed the Spooky connecting to the village.
    
    Some other minor fixes as well

--- sandbox/Assets/Scripts/Camera/OrthoShift.cs
+++ sandbox/Assets/Scripts/Camera/OrthoShift.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 
 public class OrthoShift : MonoBehaviour
 {
-	public CameraShift cameraShift;
+	public CameraShift camShift;
 	public float shiftSpeed;
 	[Tooltip("How much should the orthographic zoom in/out be. (1 min, 25 max")]
 	[Range(1.0f, 25.0f)]
 	public float shiftAmount;
 
+	public void Start()
+	{
+		camShift = GameObject.Find("Main Camera").GetComponent<CameraShift>();
+	}
 	public void OnTriggerEnter2D(Collider2D collision)
 	{
 		//Debug.Log("Entered");
 		if (collision.gameObject.name == "Knight")
 		{
-			cameraShift.SetUnshiftedOrthSize(shiftAmount);
-			cameraShift.OrthoZoom(shiftAmount, shiftSpeed);
+			camShift.SetUnshiftedOrthSize(shiftAmount);
+			camShift.OrthoZoom(shiftAmount, shiftSpeed);
 		}
 	}
 
@@ -25,8 +29,8 @@ public class OrthoShift : MonoBehaviour
 
 		if (collision.gameObject.name == "Knight")
 		{
-			cameraShift.SetUnshiftedOrthSize(5);
-			cameraShift.OrthoZoom(5, shiftSpeed);
+			camShift.SetUnshiftedOrthSize(5);
+			camShift.OrthoZoom(5, shiftSpeed);
 		}
 	}
 }
